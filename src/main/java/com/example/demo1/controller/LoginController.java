a0775bdad1168cbc9c0960dbf01b3cab2044d4dc
1024109095@qq.com <1024109095@qq.com>
2018-10-31 23:41:40 +0800

    用户登陆并获取session_key

---
+++ src/main/java/com/example/demo1/controller/LoginController.java
@@ -0,0 +1,83 @@
+package com.example.demo1.controller;
+
+import java.util.List;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.Map;
+
+import com.example.demo1.entity.wd_product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+
+public class LoginController {
+
+    @RequestMapping(value={"/user_login"}, method=RequestMethod.GET)
+    public String getProductData(String code){
+        String url = "https://api.weixin.qq.com/sns/jscode2session";
+        String appid = "wxe9f348c2692ffe5c";
+        String secret = "4461a9c9bedbf8b0cae7f1549575e9d4";
+        String param = "appid=" +appid+"&secret="+secret+"&js_code=" + (String)code +"&grant_type=authorization_code";
+        String resu =  GetRequest.sendGet(url, param);
+        System.out.println(resu);
+        return resu;
+    }
+}
+
+class GetRequest {
+
+    public static String sendGet(String url, String param) {
+        String result = "";
+        BufferedReader in = null;
+        try {
+            String urlNameString = url + "?" + param;
+            URL realUrl = new URL(urlNameString);
+            // 打开和URL之间的连接
+            URLConnection connection = realUrl.openConnection();
+            // 设置通用的请求属性
+            connection.setRequestProperty("accept", "*/*");
+            connection.setRequestProperty("connection", "Keep-Alive");
+            connection.setRequestProperty("user-agent",
+                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+            // 建立实际的连接
+            connection.connect();
+            // 获取所有响应头字段
+            Map<String, List<String>> map = connection.getHeaderFields();
+            // 遍历所有的响应头字段
+            for (String key : map.keySet()) {
+                System.out.println(key + "--->" + map.get(key));
+            }
+            // 定义 BufferedReader输入流来读取URL的响应
+            in = new BufferedReader(new InputStreamReader(
+                    connection.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                result += line;
+            }
+        } catch (Exception e) {
+            System.out.println("发送GET请求出现异常！" + e);
+            e.printStackTrace();
+        }
+        // 使用finally块来关闭输入流
+        finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+        return result;
+    }
+}
